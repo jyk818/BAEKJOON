47d5441d1184fa0aedd0b3e6f6e231040337857f
jyk818 <157190297+jyk818@users.noreply.github.com>
2024-06-12 18:43:11 +0900

    Update 팰린드롬인지 확인하기.py

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.py
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.py
@@ -1,7 +1,15 @@
-N = input()
+# Case 1 : reverse using string
+# N = input()
 
-if N == N[::-1]:
- print('1')
+# if N == N[::-1]:
+#  print('1')
+# else:
+#  print('0')
 
+ # Case 2 : reverse using list
+N = list(input())
+
+if list(reversed(N)) == N:
+  print(1)
 else:
- print('0')
+  print(0)
