a1a8df79f71ca1a6c365ab6e5d53b0a80df2fdc6
jyk818 <157190297+jyk818@users.noreply.github.com>
2024-09-10 20:05:00 +0900

    [Bronze III] Title: 중앙 이동 알고리즘, Time: 28 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.py
@@ -0,0 +1,32 @@
+N = int(input())
+
+if N == 1 :
+  print(9)
+elif N == 2 :
+  print(25)
+elif N == 3 :
+  print(81)
+elif N == 4 :
+  print(289)
+elif N == 5 :
+  print(1089)
+elif N == 6 :
+  print(4225)
+elif N == 7 :
+  print(16641)
+elif N == 8 :
+  print(66049)
+elif N == 9 :
+  print(263169)
+elif N == 10 :
+  print(1050625)
+elif N == 11 :
+  print(4198401)
+elif N == 12 :
+  print(16785409)
+elif N == 13 :
+  print(67125249)
+elif N == 14 :
+  print(268468225)
+elif N == 15 :
+  print(1073807361)
