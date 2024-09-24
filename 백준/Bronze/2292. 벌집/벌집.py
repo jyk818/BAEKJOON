a809711bcf99822c6668d86c7e4373b324e18bab
jyk818 <157190297+jyk818@users.noreply.github.com>
2024-09-24 19:51:02 +0900

    Update 벌집.py

--- 백준/Bronze/2292. 벌집/벌집.py
+++ 백준/Bronze/2292. 벌집/벌집.py
@@ -3,10 +3,23 @@ min = 1
 max = 1
 count = 1
 while True: 
-  if min <= N and N <= max:
-    print(count)
+  if min <= N and N <= max: # if N is in between this range
+    print(count)            # number of layers
     break
   else: 
-    min = max+1
-    max = max + (6 * count)
-    count += 1
+    min = max+1             # start of next layer
+    max = max + (6 * count) # end of next layer
+    count += 1              # add one more layer to the bee hive
+
+
+###
+
+# N = int(input())
+# max = 1
+# count = 1
+
+# while max < N :       # if N becomes greater than the max range
+#   max += 6 * count    # end of next layer
+#   count += 1          # another layer
+
+# print(count)
